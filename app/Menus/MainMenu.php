9599d44c1c9a536874f320d69c21a3934a61354e
William Andoh-Mensah <wandoh-mensah@itconsortiumgh.com>
2021-06-02 16:27:19 +0000

    fixed a bug

--- app/Menus/MainMenu.php
+++ app/Menus/MainMenu.php
@@ -5,14 +5,12 @@ namespace App\Menus;
 use App\ExternalServices\MomoTellerApi;
 use App\MenuContent;
 use App\MenuOptions;
-use App\Menus\SubMenus\AccountNumber;
-use App\Menus\SubMenus\MobileWalletNetwork;
-use App\Menus\SubMenus\TransactionHistory;
 use App\Responses;
 use App\State\ClientState;
 use App\State\UserState;
 use Log;
 use App\Menus\SubMenus\ConfirmFarmerName;
+
 
 class MainMenu
 {
@@ -27,10 +25,16 @@ class MainMenu
         if (!$response) {
             return $this->endSession("You are not authorised to access this service!", 'main_menu');
         }
+        if (request()->userInput == $this->CANCEL) {
+            Log::info('Confirm Main Menu - cancelled');
+            return $this->endSession("Exiting", 'main_menu');
+        }
         $userState->store($response);
         $content = $this->getMenuContent('main_menu');
         return $this->response($content,'main_menu');
     }
+
+
     public function fire($state = null, $next = null, $data = null)
     {
         Log::info('Main Menu Fired', ['state' => $state, 'next' => $next]);
@@ -41,12 +45,15 @@ class MainMenu
             $state->setState($next, request()->all(), $flow);
             return (new ConfirmFarmerName)->fire($state);
         }
-    
+
         if ($next && $next === 'main_menu' || $next === 'ROOT') {
             return $this->index();
         }
         return "Invalid Input";
-    }public function refreshSession($clientState, $userState)
+    }
+
+
+    public function refreshSession($clientState, $userState)
     {
         $userState->clearState();
         $clientState->clearState();
